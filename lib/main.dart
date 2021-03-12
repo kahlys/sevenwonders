b07075365b00dfc4e59a307acff845fbd7d4d8e9
kahlys <youennlaborde@live.fr>
2021-03-12 04:19:26 +0100

    tap outside field to unfocus and hide phone keyboard

--- lib/main.dart
+++ lib/main.dart
@@ -9,12 +9,18 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Seven Wonders Score Sheet',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return GestureDetector(
+      onTap: () {
+        // tap outside of textfields to hide the soft keyboard
+        FocusScope.of(context).requestFocus(new FocusNode());
+      },
+      child: MaterialApp(
+        title: 'Seven Wonders Score Sheet',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: ScoreSheetPage(),
       ),
-      home: ScoreSheetPage(),
     );
   }
 }
