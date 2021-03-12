c0ed6c36c1258b0ec080b354f2b032f6b66f12db
kahlys <youennlaborde@live.fr>
2021-03-12 02:03:04 +0100

    player science score

--- lib/models/player.dart
+++ lib/models/player.dart
@@ -1,3 +1,7 @@
+import 'dart:math';
+
+import 'dart:ui';
+
 class Player {
   String? name;
   int warScore = 0;
@@ -6,6 +10,12 @@ class Player {
   int commerceScore = 0;
   int guildeScore = 0;
   ScoreMoney moneyScore = ScoreMoney();
+
+  // science
+  int gears = 0;
+  int compass = 0;
+  int tablets = 0;
+  int wilds = 0;
 
   Player(String name) {
     this.name = name;
@@ -17,7 +27,19 @@ class Player {
         this.civilianScore +
         this.commerceScore +
         this.guildeScore +
-        this.moneyScore.score();
+        this.moneyScore.score() +
+        _scienceScore(this.wilds, this.compass, this.gears, this.tablets);
+  }
+
+  // _scienceScore calcultation according to wilds, compass, gears, and tablets.
+  static int _scienceScore(int w, int c, int g, int t) {
+    if (w <= 0) {
+      return (g * g) + (c * c) + (t * t) + 7 * (min(min(c, g), t));
+    }
+    return max(
+        max(_scienceScore(w - 1, c + 1, g, t),
+            _scienceScore(w - 1, c, g + 1, t)),
+        _scienceScore(w - 1, c, g, t + 1));
   }
 }
 
